36db7655baa9def705800e91d22dd4c997332cc2
Hubris of Humanity <darkerpestilence@gmail.com>
2022-09-18 12:30:15 +1000

    Add guild name to interaction log

--- bot.js
+++ bot.js
@@ -36,7 +36,7 @@ for (const file of eventFiles) {
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('Client is ready.');
 });
 
 client.on('interactionCreate', async interaction => {
